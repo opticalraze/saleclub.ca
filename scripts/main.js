0a8e6f2efc62245409a90421c60697abae9f01cd
Markus <modernfuzzball2@hotmail.com>
2021-03-24 22:22:37 -0400

    Worked on the categories section
    made the section more unified
    added a selection menu
    added cards and made them dynamic

--- scripts/main.js
+++ scripts/main.js
@@ -20,19 +20,45 @@ $(document).ready(function() {
 
     //console.log(data);
 
-    var firstLoop = true;
+    //var firstLoop = true;
+    var arrayRow = 0;
     data.dod.forEach(function(a) {
-      if (firstLoop) {
+      //if (firstLoop) {
+      if (arrayRow == 0) {
         $("#dod").append(`<div class="carousel-item active"><a href="${a.link}" target="_blank"><img class="d-block w-100" src="${a.picture}" alt="${a.brand}"></a></div>`);
-        firstLoop = false;
+        $("#dodIndicators").append(`<li data-target="#carouselExampleIndicators" data-slide-to="${arrayRow}" class="active"></li>`);
+        arrayRow++;
       } else {
         $("#dod").append(`<div class="carousel-item"><a href="${a.link}" target="_blank"><img class="d-block w-100" src="${a.picture}" alt="${a.brand}"></a></div>`);
+        $("#dodIndicators").append(`<li data-target="#carouselExampleIndicators" data-slide-to="${arrayRow}"></li>`);
+        arrayRow++;
       }
     });
 
     //firstLoop = true;
-    data.tech.forEach(function(a) {
+    /*data.categories.forEach(function(a) {
+
         $("#tech").append(`<div style="display:inline-block;margin:10px;"><a href="${a.link}" target="_blank"><img width="350" src="${a.picture}" alt="${a.brand}"></a></div>`);
+    });*/
+
+    data.tech.forEach(function(a) {
+
+      var brandlogo = a.brand.toLowerCase().replace(/\s+/g, '');
+
+      $("#tech").append(`
+        <div class="card grow sale">
+          <a class="text-dark" href="${a.link}" target="_blank">
+            <div class="card-img-top">
+              <img class="wide" src="${a.picture}" alt="${a.brand}">
+            </div>
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <img class="brand-logo" src="assets/affiliates/${brandlogo}.svg">
+          </a>
+        </div>
+      `);
+        //$("#tech").append(`<div style="display:inline-block;margin:10px;"><a href="${a.link}" target="_blank"><img width="350" src="${a.picture}" alt="${a.brand}"></a></div>`);
     });
   });
 
